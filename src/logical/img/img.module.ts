556c7ad2c198e11ad6b8a2a414c4d86942ae0224
tcguoxing <369552300@qq.com>
2024-11-30 16:33:12 +0800

    MOD:创建有关图像上传的功能;

--- src/logical/img/img.module.ts
+++ src/logical/img/img.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ImgController } from './img.controller';
 import { ImgService } from './img.service';
 
+// 创建有关图像上传下载的功能；
 @Module({
   // 基本的控制器
   controllers: [ImgController],
