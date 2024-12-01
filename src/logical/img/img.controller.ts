bf6da021ecb0bec78143d9d4dd2aed954b5b6841
tcguoxing <369552300@qq.com>
2024-12-02 02:12:10 +0800

    MOD：修复imgController中的一个错误;

--- src/logical/img/img.controller.ts
+++ src/logical/img/img.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { ImgService } from './img.service';
 import { UserService } from '../user/user.service';
 
